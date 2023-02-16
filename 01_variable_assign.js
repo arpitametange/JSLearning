4f7b8af317e448d70337a8ec14651156a0c8220f
arpitametange <arpitametange@gmail.com>
2023-02-16 09:40:20 +0530

    JS's first task done

---
+++ 01_variable_assign.js
@@ -0,0 +1,24 @@
+console.log('*****************This is the first step*************************');
+var myFullName;
+console.log("___VARIABLE DECLARED___");
+console.log('My full name is :', myFullName);
+myFullName='Arpita Metange'
+console.log('___VARIABLE INTIALIZED___');
+console.log("MY Full Name is :",myFullName);
+myFullName='Arpita Vilasrao Metange'
+console.log("___UPDATED THE VARIABLE___");
+console.log("My full name is:",myFullName)
+console.log('*****************This is the Second step*************************');
+var myLove="I love only JS"
+console.log('___DECLAIRED AND INITIALIZED A VARIABLE___');
+console.log("My love:",myLove);
+console.log('*****************This is the Third step*************************');
+var firstName="Arpita"
+var lastName='Metange'
+var age='23'
+var address='Amravati'
+var pincode='444606'
+var gender='Female'
+var isMarried='No'
+console.log('___DECLAIRED AND INITIALIZED ON THE SAME LINE___')
+console.log('First name is:',firstName,', Last name is:',lastName,', My age is :',age, ", My address is :", address,", Pincode:",pincode,", Gender:",gender, ",  Marital status :" , isMarried);
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,7 @@
 console.log('arpita metange');
+// console.log(24);
+var fullName="arpita metange"
+console.log(fullName);
+
+var cuty;
+console.log(cuty);
