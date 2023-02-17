e3423a522bb429a6c8dd93bbba165877d2733d89
arpitametange <arpitametange@gmail.com>
2023-02-17 11:06:52 +0530

    Done with the js2 assigments

---
+++ 01_variableBasicsAssig.js
@@ -0,0 +1,14 @@
+var Marks=89;
+var str='This is the string';
+var fresher=true;
+console.log('**********Example of Number-datatype*********');
+console.log('Marks:',Marks, ', Type:',typeof(Marks));
+console.log('**********Example of  String-datatype*********');
+console.log("String:",str , ', Type:',typeof(str));
+console.log('**********Example of  Boolean-datatype*********');
+console.log('Are you a fresher? :',fresher, ', Type:',typeof(fresher));
+console.log('*******Difference between the null and undefined*********');
+var differenceNull='When there is no value then it is called null . Its type is object'
+var differenceUndefined='Variable is undefined when we do not assigned a value to the variable after declaring it. Its type is undefined'
+console.log('Null :',differenceNull);
+console.log('Undefined',differenceUndefined);
---
+++ 02_typeOf.js
@@ -0,0 +1,26 @@
+//'********1st step********'// 
+var bankName;
+console.log('************Declaration**********');
+console.log('bankName:',bankName, ', Type of variable:',typeof(bankName));
+bankName="SBI Bank";
+console.log('**********Initialization**********');
+console.log('Bank name:',bankName,', Type of variable :' ,typeof(bankName));
+// ********2nd step********//
+
+var Marks='90%';
+console.log('************Declaration and Initialization**********');
+console.log('Marks:',Marks, ', Type of variable:',typeof(Marks));
+// ********3rd step********//
+var isWorking;
+console.log('************Declaration**********');
+console.log('isWorking:', isWorking);
+isWorking=true;
+console.log('**********Initialization**********');
+console.log('Type of variable:', typeof(isWorking));
+// ********4th step********//
+var totalCount='Hundred and Seven'
+console.log('************Declaration and Initialization**********');
+console.log('total count=',totalCount, ', datatype=',typeof(totalCount));
+console.log('************Updating variable**********');
+totalCount=107;
+console.log('total count=',totalCount, ', datatype=',typeof(totalCount));
