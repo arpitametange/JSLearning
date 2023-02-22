4167c0a7ddb6ce9f1e11bde074dd23a1ff2a2bcb
arpitametange <arpitametange@gmail.com>
2023-02-22 11:19:52 +0530

    operator assignment done

---
+++ 04_stringTemplate.js
@@ -0,0 +1,12 @@
+
+//string template
+console.log(`you are a "champ"`);
+
+///variable substitution
+
+var fistName="arpita";
+var lastName="metange";
+var city="amravati";
+
+console.log("My First name ", fistName, "Last name ", lastName, " City ", city);
+console.log(`My first name is ${fistName} My last name is : ${lastName} my city ${city}`);
---
+++ 05_operatorA.js
@@ -0,0 +1,35 @@
+console.log(`*****This is the first step*****`);
+function sqareOfWordLength (word){
+   console.log(`Word length is ${word.length}`); 
+     return word.length**2;
+    
+}
+console.log(`//Length of javascript//`);
+ var wordlength=sqareOfWordLength( "JavaScript");
+ console.log(`The square of string Javascript is ${wordlength} `);
+ console.log(`//Length of Google//`);
+ var wordGoogle=sqareOfWordLength("Google Chrome");
+  console.log(`The square of string  google is ${wordGoogle}`);
+  console.log(`//Length of DeveloperSmart//`);
+  var wordDeveloperSmart=sqareOfWordLength("Developer Smart");
+  console.log(`The square of string  developerSmart is ${wordDeveloperSmart}`);
+
+console.log(`*****This is the second step*****`);
+function str(){
+    console.log(`The string is : I am Angular Developer`);
+     
+}
+str()
+var len=`I am Angular Developer`
+var lengthstr=len.length;
+console.log(`Length of sting = ${lengthstr}`);
+var spl=len.split(" ")
+console.log(`Length of total words in string = ${spl.length}`);
+var div=lengthstr/spl.length;
+console.log(`String length divided by total number of words in string = ${div}`);
+var multiplystr=lengthstr*spl.length;
+console.log(`Multiplication of string length and total words available = ${multiplystr}`);
+
+
+
+
