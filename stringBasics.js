3639b267975d05914f0b72b427eccd2bd7453648
arpitametange <arpitametange@gmail.com>
2023-02-23 11:40:05 +0530

    Update stringBasics.js

--- stringBasics.js
+++ stringBasics.js
@@ -3,17 +3,12 @@ function  stringBasics(){
 }
 stringBasics()
 
-var myHobby1="Exploring new things ,";
-var myHobby2="Drawing, ";
-var myHobby3="Workout"
-console.log("My hobbies are ",myHobby1,myHobby2,myHobby3);
-console.log('characters presents in hobby1:',myHobby1.length);
-console.log('characters presents in hobby2:',myHobby2.length);
-console.log('characters presents in hobby3:',myHobby3.length);
 function sumofchr(myHobby1,myHobby2,myHobby3){
-    var sum=myHobby1+myHobby2+myHobby3;
+    console.log(myHobby1,myHobby2,myHobby3);
+
+    var sum=myHobby1.length+myHobby2.length+myHobby3.length;
     return sum;
 }
-var total=sumofchr(20,7,7);
+var total=sumofchr('reading',"writing",'playing');
 console.log("Total length of three variable :" ,total);
 
