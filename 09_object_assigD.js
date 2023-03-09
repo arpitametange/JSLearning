b4828b8c887b5ba5b0ce46b65ace77bbcab93f5a
arpitametange <arpitametange@gmail.com>
2023-03-09 16:12:51 +0530

    update the assignment

--- 09_object_assigD.js
+++ 09_object_assigD.js
@@ -6,34 +6,41 @@ let professor={
     number:8933849929,
     address:"samara nagar",
     state:"Maharashtra",
-    country:"India",
-    degrees:{
-        engineering:"CSC",
-        PHD:"Adv Computing"
-    },
-    certificates:["Hacker Rank Participation", "Certificate in IFE course","Certificate in Adv Programming"],
-    totalDegrees:function () {
-        return `1)Teacher's engineering  degree in  ${this.degrees.engineering} and phd in ${this.degrees.PHD}`
-    },
-    totalExperience:"14 years"
+    country:"India",  
+}
+console.log("***********************Properties added to the object ******************************");
+console.log(professor);
+console.log("***********************Nested object 'degrees' added to the object******************************");
+professor. degrees={
+    engineering:"CSC",
+    PHD:"Adv Computing"
+}
+console.log(professor.degrees);
+console.log("***********************array certificate added to the object ******************************");
+professor. certificates=["Hacker Rank Participation", "Certificate in IFE course","Certificate in Adv Programming"]
+console.log(professor.certificates);
+
+console.log("***********************Function added to the object ******************************");
+
+professor. totalDegrees=function () {
+    return `Teacher's engineering  degree in  ${this.degrees.engineering} and phd in ${this.degrees.PHD}`
 }
 var result=professor.totalDegrees();
 console.log(result);
-console.log(`2)Total experience are ${professor.totalExperience}`);
-console.log(professor);
+professor.totalExperience=14
+console.log(`Total experience are ${professor.totalExperience}`);
 professor.city="pune"
-console.log(`3)City updated to  ${professor.city}`);
+console.log(`City updated to  ${professor.city}`);
 professor.id=25
-console.log(`4)Id is updated to ${professor.id}`);
+console.log(`Id is updated to ${professor.id}`);
 console.log(professor);
 delete professor.number
-console.log(`5)Number is deleted from the object`);
+console.log(`Number is deleted from the object`);
 console.log(professor);
-
 console.log("**********************Before adding new certificate in array****************************");
-console.table( professor.certificates);
-console.log("**********************6)After adding new certificate in array****************************");
-professor.certificates.splice(professor.certificates.length,1,"Oracle Certified")
-console.table( professor.certificates);
+console.log( professor.certificates);
+console.log("**********************After adding new certificate in array****************************");
+professor.certificates.push("Oracle Certified")
+console.log( professor.certificates);
 console.log(`Last element of the array-certificate is = ${professor.certificates[professor.certificates.length-1]}`);
 
